4a76ed58382d643317f9232fa2c3f5aed62f686d
Marcio Nedel <nomanedel2@gmail.com>
2025-08-07 18:59:52 -0300

    site online

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
@@ -9,9 +9,9 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <HashRouter>
+  <App />
+</HashRouter>
     </StrictMode>
   );
 } else {
